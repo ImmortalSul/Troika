5c9699c45d01b4b2d1363d3069800c896c015647
ImmortalSul <157292013+ImmortalSul@users.noreply.github.com>
2024-11-19 17:41:38 +0530

    Main struct of site mostly un changed

---
+++ src/app/(marketing)/floatingDock/page.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+import { FloatingDock } from "@/components/ui/floating-dock";
+import {
+  IconBrandGithub,
+  IconBrandLinkedin,
+  IconBrandX,
+  IconExchange,
+  IconHome,
+  IconNewSection,
+  IconTerminal2,
+} from "@tabler/icons-react";
+import Image from "next/image";
+
+export function FloatingDockDemo() {
+  const links = [
+    {
+      title: "Home",
+      icon: (
+        <IconHome className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "#",
+      target: "_blank",
+    },
+
+    {
+      title: "Products",
+      icon: (
+        <IconTerminal2 className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "/products",
+      target: "_self",
+    },
+    
+    {
+      title: "LinkedIn",
+      icon: (
+        <IconBrandLinkedin className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "https://www.linkedin.com/company/troika-hub",
+      target: "_blank",
+    },
+
+    {
+      title: "Twitter",
+      icon: (
+        <IconBrandX className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "#",
+      target: "_blank",
+    },
+    {
+      title: "GitHub",
+      icon: (
+        <IconBrandGithub className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "https://github.com/Troika-Hub",
+      target: "_blank",
+    },
+  ];
+  return (
+    <div className="flex items-center justify-center h-[10rem] w-full">
+      
+      <FloatingDock
+        items={links}
+        
+      />
+      
+    </div>
+  );
+}
+
---
+++ src/utils/constants/pricing.ts
@@ -0,0 +1,198 @@
+// export const PLANS = [
+//     {
+//         name: "Free",
+//         info: "For most individuals",
+//         price: {
+//             monthly: 0,
+//             yearly: 0,
+//         },
+//         Products: [
+//             { text: "Shorten links" },
+//             { text: "Up to 100 tags", limit: "100 tags" },
+//             { text: "Customizable branded links" },
+//             { text: "Track clicks", tooltip: "1K clicks/month" },
+//             { text: "Community support", tooltip: "Get answers your questions on discord" },
+//             { text: "AI powered suggestions", tooltip: "Get up to 100 AI powered suggestions" },
+//         ],
+//         btn: {
+//             text: "Start for free",
+//             href: "/auth/sign-up?plan=free",
+//             variant: "default",
+//         }
+//     },
+//     {
+//         name: "Pro",
+//         info: "For small businesses",
+//         price: {
+//             monthly: 9,
+//             yearly: 90,
+//         },
+//         Products: [
+//             { text: "Shorten links" },
+//             { text: "Up to 500 tags", limit: "500 tags" },
+//             { text: "Customizable branded links" },
+//             { text: "Track clicks", tooltip: "20K clicks/month" },
+//             { text: "Export click data", tooltip: "Upto 1K links" },
+//             { text: "Priority support", tooltip: "Get 24/7 chat support" },
+//             { text: "AI powered suggestions", tooltip: "Get up to 500 AI powered suggestions" },
+//         ],
+//         btn: {
+//             text: "Get started",
+//             href: "/auth/sign-up?plan=pro",
+//             variant: "purple",
+//         }
+//     },
+//     {
+//         name: "Business",
+//         info: "For large organizations",
+//         price: {
+//             monthly: 49,
+//             yearly: 490,
+//         },
+//         Products: [
+//             { text: "Shorten links" },
+//             { text: "Unlimited tags" },
+//             { text: "Customizable branded links"},
+//             { text: "Track clicks", tooltip: "Unlimited clicks" },
+//             { text: "Export click data", tooltip: "Unlimited clicks" },
+//             { text: "Dedicated manager", tooltip: "Get priority support from our team" },
+//             { text: "AI powered suggestions", tooltip: "Get unlimited AI powered suggestions" },
+//         ],
+//         btn: {
+//             text: "Contact team",
+//             href: "/auth/sign-up?plan=business",
+//             variant: "default",
+//         }
+//     }
+// ];
+
+// export const PRICING_Products = [
+//     {
+//         text: "Shorten links",
+//         tooltip: "Create shortened links",
+//     },
+//     {
+//         text: "Track clicks",
+//         tooltip: "Track clicks on your links",
+//     },
+//     {
+//         text: "See top countries",
+//         tooltip: "See top countries where your links are clicked",
+//     },
+//     {
+//         text: "Upto 10 tags",
+//         tooltip: "Add upto 10 tags to your links",
+//     },
+//     {
+//         text: "Community support",
+//         tooltip: "Community support is available for free users",
+//     },
+//     {
+//         text: "Priority support",
+//         tooltip: "Get priority support from our team",
+//     },
+//     {
+//         text: "AI powered suggestions",
+//         tooltip: "Get AI powered suggestions for your links",
+//     },
+// ];
+
+// export const WORKSPACE_LIMIT = 2;
+export const PLANS = [
+    {
+        name: "Co-Founder",
+        info: "Lead Creative Department",
+        price: {
+            monthly: "Chaitayna ",
+            
+        },
+        Products: [
+            { text: "Touches Children" },
+            { text: "Gets all the bitches", tooltip:"got 1 bitch"},
+            { text: "Mastermind skibidi gyatt", tooltip:"6'2 btw" },
+            { text: "Tracks children home", tooltip: "17 children missing last night" },
+            { text: "Doesnt like black people", tooltip: "Trump got elected" },
+            { text: "Watches too many reels", tooltip: "Likes eating watermelon" },
+        ],
+        btn: {
+            text: "Contact",
+            href: "",//portfolio
+            variant: "default",
+        }
+    },
+    {
+        name: "Founder",
+        info: "Lead Dev + CEO",
+        price: {
+            monthly: "S Sulaiman",
+            
+        },
+        Products: [
+            { text: "Lead Developer & Creator" },
+            { text: "4+ Years of Experience", tooltip:"Specialized in building SaaS platforms and Designing Websites"},
+            { text: "Projects Built", tooltip:"Inquirix, Optix, Conferix, and other innovative tech solutions." },
+            { text: "Previous Roles", tooltip: "Interned at Deloitte and Capgemini; Worked at early stage governament funded startups." },
+            { text: "Full Stack Dev",},
+            { text: "Tech Stack", tooltip: "TypeScript React TRPC AWS Azure Next.js Rust/Solana Three.js GoLang and Various Databases and ORMs" },
+        ],
+        btn: {
+            text: "Contact",
+            href: "",//Portfolio
+            variant: "purple",
+        }
+    },
+    {
+        name: "Co-Founder",
+        info: "Founding Engineer",
+        price: {
+            monthly: "Pratyush Bose",
+            yearly: Math.round(49 * 12 * (1 - 0.12)),
+        },
+        Products: [
+            { text: "Touches Children" },
+            { text: "Gets all the bitches", tooltip:"got 1 bitch"},
+            { text: "Mastermind skibidi gyatt", tooltip:"6'2 btw" },
+            { text: "Tracks children home", tooltip: "17 children missing last night" },
+            { text: "Doesnt like black people", tooltip: "Trump got elected" },
+            { text: "Watches too many reels", tooltip: "Likes eating watermelon" },
+        ],
+        btn: {
+            text: "Contact",
+            href: "",//portfolio
+            variant: "default",
+        }
+    }
+];
+
+export const PRICING_Products = [
+    {
+        text: "Shorten links",
+        tooltip: "Create shortened links",
+    },
+    {
+        text: "Track clicks",
+        tooltip: "Track clicks on your links",
+    },
+    {
+        text: "See top countries",
+        tooltip: "See top countries where your links are clicked",
+    },
+    {
+        text: "Upto 10 tags",
+        tooltip: "Add upto 10 tags to your links",
+    },
+    {
+        text: "Community support",
+        tooltip: "Community support is available for free users",
+    },
+    {
+        text: "Priority support",
+        tooltip: "Get priority support from our team",
+    },
+    {
+        text: "AI powered suggestions",
+        tooltip: "Get AI powered suggestions for your links",
+    },
+];
+
+export const WORKSPACE_LIMIT = 2;
